84bd49ccfaf498bac0467a300796bf47de8106f5
tsm <422933768@qq.com>
2023-12-08 17:36:39 +0800

    升级到apktool V2.9.1 修改原作目录及步骤流程

--- ApkTool.py
+++ ApkTool.py
@@ -1,12 +1,13 @@
 # coding=utf-8
 import os
-import sys
+import shutil
 import argparse
 from subprocess import Popen, PIPE
 
 import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+import importlib
+
+importlib.reload(sys)
 
 
 class ApkTool:
@@ -18,44 +19,54 @@ class ApkTool:
         path = ''
         if hasattr(sys, '_MEIPASS'):
             path = sys._MEIPASS + self.file_separator
-        self.apktooljar = path + 'apktool.jar'
+        self.apk_tool_jar = path + 'apktool_2.9.1.jar'
         self.aapt = path + 'aapt.exe'
         self.objdump_x86 = path + 'objdump_x86.exe'
         self.objdump_arm = path + 'objdump_arm.exe'
         if keystore is None:
-            self.keystore = path + 'mykey-123456.keystore'
+            self.keystore = path + 'test_abc123'
         else:
             self.keystore = keystore
         if password is None:
-            self.password = '123456'
+            self.password = 'abc123'
         else:
             self.password = password
         if alias is None:
-            self.alias = 'mykey'
+            self.alias = 'abc123'
         else:
             self.alias = alias
         self.cur_apk = {}
         return
 
     def unpack(self, apk, path):
-        cmd = 'java -jar ' + self.apktooljar + (' d -f -o %s %s' % (path, apk))
+        cmd = 'java -jar ' + self.apk_tool_jar + (' d -f -o %s %s' % (path, apk))
+        print("unpack命令:", cmd)
         os.system(cmd)
+        print("unpack完成!!!")
         return
 
     def pack(self, path, apk):
-        cmd = 'java -jar ' + self.apktooljar + (' b %s -o %s' % (path, apk))
+        cmd = 'java -jar ' + self.apk_tool_jar + (' b %s -o %s' % (path, apk))
+        print("pack命令:", cmd)
         os.system(cmd)
+        print("pack完成!!!")
         return
 
-    def sign(self, apk, signed_apk):
-        path, file = os.path.split(signed_apk)
-        if os.path.exists(path) is False:
-            os.makedirs(path)
-        keystore = ' -keystore %s -storepass %s' % (self.keystore, self.password)
-        signedjar = ' -signedjar %s %s -digestalg SHA1 -sigalg MD5withRSA %s' % (signed_apk, apk, self.alias)
-        cmd = 'jarsigner -verbose ' + keystore + signedjar
-        print(cmd)
+    def sign_with_apk_signer(self, apk_path, signed_apk_path):
+        keystore = '--ks %s --ks-key-alias %s --ks-pass pass:%s' % (self.keystore, self.alias, self.password)
+        cmd = "apksigner sign %s --in %s --out %s" % (keystore, apk_path, signed_apk_path)
+        print("签名命令:", cmd)
         os.system(cmd)
+        print("签名结束!!!")
+        return
+
+    @staticmethod
+    def zipalign(apk_path, apk_align_path):
+        cmd = 'zipalign -p -f -v 4 %s %s' % (apk_path, apk_align_path)
+        print("对齐命令:", cmd)
+        os.system(cmd)
+        print("对齐结束!!!")
+        return
 
     def get_apk_label(self, apk_path):
         pipe = Popen([self.aapt, 'dump', 'badging', apk_path], stdout=PIPE)
@@ -92,8 +103,8 @@ def get_game_engine(libpath, file_separator):
         'libunity.so': 'unity3D引擎',
         'libgdx.so': 'libgdx引擎'
     }
-    dir = ['armeabi-v7a', 'armeabi', 'x86']
-    for d in dir:
+    arch_dir = ['armeabi-v7a', 'armeabi', 'x86']
+    for d in arch_dir:
         lib = libpath + d + file_separator
         for f in data.keys():
             if os.path.exists(lib + f):
@@ -101,116 +112,67 @@ def get_game_engine(libpath, file_separator):
     return '未知引擎'
 
 
-def analyse(apk, tool):
+def analyse(apk, tool, step):
     path, file = os.path.split(apk)
     if len(path) == 0:
         path = '.'
     out_name = file[:-4]
-    out_txt = out_name + '.txt'
-    f_out = open(out_txt, 'w+')
-    line = '文件名称：%s\n' % apk
-    f_out.write(line)
-    line = '应用名称：%s\n' % tool.get_apk_label(apk)
-    f_out.write(line)
-    line = '应用信息：%s\n' % tool.get_apk_package_name(apk)
-    f_out.write(line)
+    print("开始处理:", out_name)
     unpack_path = path + tool.file_separator + 'unpack' + tool.file_separator + out_name
-    if os.path.exists(unpack_path) is False:
-        os.makedirs(unpack_path)
-    repack_path = path + tool.file_separator + 'repack' + tool.file_separator + out_name + '.apk'
-    if os.path.exists(path + tool.file_separator + 'repack') is False:
-        os.makedirs(path + tool.file_separator + 'repack')
-    sign_path = path + tool.file_separator + 'sign' + tool.file_separator + out_name + '.apk'
-    if os.path.exists(path + tool.file_separator + 'sign') is False:
-        os.makedirs(path + tool.file_separator + 'sign')
-    if os.path.exists(unpack_path + tool.file_separator + 'lib') is False:
+    repack_apk_path = path + tool.file_separator + 'out' + tool.file_separator + out_name + '_first.apk'
+    align_apk_path = path + tool.file_separator + 'out' + tool.file_separator + out_name + '_align.apk'
+    sign_path = path + tool.file_separator + 'out' + tool.file_separator + out_name + '_sign.apk'
+    # apktool打包
+    if os.path.exists(path + tool.file_separator + 'out') is False:
+        os.makedirs(path + tool.file_separator + 'out')
+        print("创建out目录")
+    if step == 0:
+        if os.path.exists(unpack_path):
+            shutil.rmtree(unpack_path)
+            os.makedirs(unpack_path)
+            print("创建upack目录")
         tool.unpack(apk, unpack_path)
-    if os.path.exists(repack_path) is False:
-        tool.pack(unpack_path, repack_path)
-    if os.path.exists(sign_path) is False:
-        tool.sign(repack_path, sign_path)
-    if os.path.exists(repack_path) is False:
-        line = '打包检测：重打包失败，无法重打包\n'
-    else:
-        line = '打包检测：重打包成功\n'
-        is_repack_ok = True
-    f_out.write(line)
-    if os.path.exists(sign_path) is False:
-        line = '签名检测：重签名失败，无法重签名\n'
-    else:
-        line = '签名检测：重签名成功\n'
-    f_out.write(line)
-    libpath = unpack_path + tool.file_separator
-    libpath += 'lib' + tool.file_separator
-    line = '引擎检测：%s\n' % get_game_engine(libpath, tool.file_separator)
-    f_out.write(line)
-    f_out.write(
-        '----------------------------------------------------------------------------------------------------------------------------------\n')
-    f_out.close()
-    pass
+    if step <= 1:
+        if os.path.exists(repack_apk_path) is True:
+            os.remove(repack_apk_path)
+            print("删除旧的apktool d 生成的apk")
+        tool.pack(unpack_path, repack_apk_path)
+    if step <= 2:
+        # zipalign 对齐
+        if os.path.exists(align_apk_path) is True:
+            os.remove(align_apk_path)
+            print("删除旧的对齐apk")
+        tool.zipalign(repack_apk_path, align_apk_path)
+        # apksigner 签名
+        if os.path.exists(sign_path) is True:
+            os.remove(sign_path)
+            print("删除旧的签名apk")
+        tool.sign_with_apk_signer(align_apk_path, sign_path)
+        # 自动安装
+        print("开始自动安装签名apk:", sign_path)
+        install_cmd = 'adb install -r %s' % sign_path
+        os.system(install_cmd)
 
 
 def main():
     parser = argparse.ArgumentParser(prog=sys.argv[0], usage='%(prog)s [options]')
     help = """help 或者 -h 显示本帮助文档 """
     parser.add_argument('-help', help=help, action='store_const', const='help')
-    parser.add_argument('-keystore', nargs='?', help='指定签名文件，默认mykey-123456.keystore')
-    parser.add_argument('-passwd', nargs='?', help='指定签名密码，默认123456')
-    parser.add_argument('-alias', nargs='?', help='指定签名别名，默认mykey')
-    parser.add_argument('-label', help='获取包名', action='store_const', const='label')
-    parser.add_argument('-unpack', help='解包文件', action='store_const', const='unpack')
-    parser.add_argument('-pack', help='打包文件', action='store_const', const='pack')
-    parser.add_argument('-sign', help='签名文件', action='store_const', const='sign')
     parser.add_argument('-analyse', help='分析包', action='store_const', const='analyse')
+    parser.add_argument('-step', nargs='?', type=int, help='开始步骤')
     parser.add_argument('-inapk', nargs='?', help='指定输入apk路径')
-    parser.add_argument('-outapk', nargs='?', help='指定输出apk路径')
-    parser.add_argument('-outpath', nargs='?', help='指定输出目录')
-    parser.add_argument('-inpath', nargs='?', help='指定输入目录')
     args = parser.parse_args()
     if args.help is not None:
         parser.print_help()
         return
-    attrs = ['keystore', 'passwd', 'alias', 'inapk', 'outapk', 'inpath', 'outpath', 'help']
+    attrs = ['inapk', 'help', 'step']
     value_map = {}
     for attr in attrs:
         value_map[attr] = getattr(args, attr, None)
-    tool = ApkTool(value_map['keystore'], value_map['passwd'], value_map['alias'])
-    if args.unpack is not None:
-        # -unpack -inapk D:\bamenGame\测试游戏\17.12.18jhzd.apk -outpath D:\bamenGame\测试游戏\out\17.12.18jhzd
-        if value_map['inapk'] is None:
-            print('需要指定输入apk路径')
-            return
-        if value_map['outpath'] is None:
-            print('需要指定输出目录')
-            return
-        tool.unpack(value_map['inapk'], value_map['outpath'])
-        return
-    if args.pack is not None:
-        # -pack -outapk D:\bamenGame\测试游戏\repack\17.12.18jhzd.apk -inpath D:\bamenGame\测试游戏\out\17.12.18jhzd
-        if value_map['inpath'] is None:
-            print('需要指定输入目录')
-            return
-        if value_map['outapk'] is None:
-            print('需要指定输出包路径')
-            return
-        tool.pack(value_map['inpath'], value_map['outapk'])
-        return
-    if args.sign is not None:
-        # -sign -inapk D:\bamenGame\测试游戏\repack\17.12.18jhzd.apk -outapk D:\bamenGame\测试游戏\sign\17.12.18jhzd.apk
-        if value_map['inapk'] is None:
-            print('需要指定输入目录')
-            return
-        if value_map['outapk'] is None:
-            print('需要指定输出包路径')
-            return
-        tool.sign(value_map['inapk'], value_map['outapk'])
-        return
-    if args.label is not None:
-        if value_map['inapk'] is None:
-            print('需要指定输入游戏包')
-            return
-        print(tool.get_apk_label(value_map['inapk']))
-        return
+    tool = ApkTool()
+    step = value_map['step']
+    print("开始步骤:", step)
+
     if args.analyse is not None:
         if value_map['inapk'] is None:
             print('需要指定输入游戏包，现在分析当前目录下所有的apk文件')
@@ -218,9 +180,9 @@ def main():
                 if os.path.isdir(file):
                     continue
                 if os.path.splitext(file)[1] == '.apk':
-                    analyse(file, tool)
+                    analyse(file, tool, step)
             return
-        analyse(value_map['inapk'], tool)
+        analyse(value_map['inapk'], tool, step)
         return
     parser.print_help()
 
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # coding=utf-8
 # 导入系统命令
+import argparse
 import os
 
 
 # 对 APK 文件进行批处理
-def batch_apk():
+def batch_apk(from_step=0):
     # 列出 apk 目录下的所有文件
     for f in os.listdir('apk'):
         # 文件名长度超过 4 个字符
@@ -12,9 +13,14 @@ def batch_apk():
             # 从后面 4 字节到结尾是 .apk ,
             # 则该文件是 APK 文件 , 对该文件进行解包
             if f[-4:] == '.apk':
-                os.system('python ApkTool.py -analyse -inapk apk/' + f)
+                os.system('python ApkTool.py -analyse -step %s -inapk apk/%s' % (from_step, f))
+                # 只处理一个apk
+                break
 
 
 # 主函数入口
 if __name__ == '__main__':
-    batch_apk()
+    parse = argparse.ArgumentParser()
+    # 默认从第一步开始(apk打包),因为第一步解包可能有很多文件，较耗时，需要时从解包开始指定-s 0
+    parse.add_argument('-s', '--step', dest='step', type=int, help='从哪一步开始,0=从解apk开始,1=从压apk开始,2=从对齐签名开始', default=1)
+    batch_apk(parse.parse_args().step)
